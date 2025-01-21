f8b8a9050fd2bacfb4110d68814083f2a5e6f641
pajicadvance <pavle.pajic23@gmail.com>
2025-01-21 02:41:36 +0100

    Config strings, conditional loot

--- src/main/java/me/pajic/simple_smithing_overhaul/loot/ModLootEvents.java
+++ src/main/java/me/pajic/simple_smithing_overhaul/loot/ModLootEvents.java
@@ -1,5 +1,6 @@
 package me.pajic.simple_smithing_overhaul.loot;
 
+import me.pajic.simple_smithing_overhaul.Main;
 import me.pajic.simple_smithing_overhaul.items.ModItems;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
@@ -12,13 +13,13 @@ public class ModLootEvents {
     public static void init() {
         LootTableEvents.MODIFY.register((resourceKey, builder, lootTableSource, provider) -> {
             if (lootTableSource.isBuiltin()) {
-                if (BuiltInLootTables.END_CITY_TREASURE.equals(resourceKey)) {
+                if (BuiltInLootTables.END_CITY_TREASURE.equals(resourceKey) && Main.CONFIG.enchantmentUpgrading.enableEnchantmentUpgrading()) {
                     builder.pool(LootPool.lootPool()
                             .add(LootItem.lootTableItem(ModItems.ENCHANTMENT_UPGRADE_SMITHING_TEMPLATE).setWeight(10))
                             .add(EmptyLootItem.emptyItem().setWeight(90)).build()
                     );
                 }
-                if (BuiltInLootTables.ANCIENT_CITY.equals(resourceKey)) {
+                if (BuiltInLootTables.ANCIENT_CITY.equals(resourceKey) && Main.CONFIG.pinnacleEnchantment.enablePinnacleEnchantment()) {
                     builder.pool(LootPool.lootPool()
                             .add(LootItem.lootTableItem(ModItems.PINNACLE_ENCHANTMENT_SMITHING_TEMPLATE).setWeight(10))
                             .add(EmptyLootItem.emptyItem().setWeight(90)).build()
