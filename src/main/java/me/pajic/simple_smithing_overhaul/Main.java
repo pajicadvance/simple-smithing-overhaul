b37fa19f5a40221b7432023a73dce3ec1ded4c66
pajicadvance <pavle.pajic23@gmail.com>
2025-03-24 21:18:26 +0100

    Use proper whetstone item repair recipe implementation instead of mixin

--- src/main/java/me/pajic/simple_smithing_overhaul/Main.java
+++ src/main/java/me/pajic/simple_smithing_overhaul/Main.java
@@ -1,15 +1,29 @@
 package me.pajic.simple_smithing_overhaul;
 
 import me.pajic.simple_smithing_overhaul.config.ModConfig;
-import me.pajic.simple_smithing_overhaul.datapacks.NetheriteRepairMaterial;
 import me.pajic.simple_smithing_overhaul.items.ModItems;
 import me.pajic.simple_smithing_overhaul.loot.ModLootEvents;
+import me.pajic.simple_smithing_overhaul.recipe.WhetstoneRepairItemRecipe;
 import me.pajic.simple_smithing_overhaul.util.ModUtil;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+//? if <= 1.21.1
+import net.minecraft.world.item.crafting.SimpleCraftingRecipeSerializer;
+//? if > 1.21.1 {
+/*import me.pajic.simple_smithing_overhaul.datapacks.NetheriteRepairMaterial;
+import net.minecraft.world.item.crafting.CustomRecipe;
+*///?}
 
 public class Main implements ModInitializer {
 
     public static ModConfig CONFIG = ModConfig.createAndLoad();
+    public static RecipeSerializer<WhetstoneRepairItemRecipe> WHETSTONE_REPAIR_ITEM = RecipeSerializer.register(
+            "crafting_special_whetstone_repairitem",
+            //? if <= 1.21.1
+            new SimpleCraftingRecipeSerializer<>(WhetstoneRepairItemRecipe::new)
+            //? if > 1.21.1
+            /*new CustomRecipe.Serializer<>(WhetstoneRepairItemRecipe::new)*/
+    );
 
     @Override
     public void onInitialize() {
